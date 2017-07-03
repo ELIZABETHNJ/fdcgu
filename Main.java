d7337ec6809e283317dd8e2f6b15b9967ca990f1
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-03 22:16:28 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+# fdcgu
+import java.io.*;
+import java.util.*;
+public class Main
+{
+public static void main(String args[])
+{
+int x;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+x=sc.nextInt();
+if(x>0)
+{
+System.out.println("The given numder is positive");
+}
+else
+{
+System.out.println("The given number is negative");
+}
+}
+}
